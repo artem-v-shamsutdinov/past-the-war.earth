a9b2f3454d4ea4be09d36e6186da09b202b01ec4
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2024-02-12 12:24:39 -0700

    Coding and language fixes.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from 'express'
-
-
+import serveIndex from 'serve-index'
 const app = express()
-app.use(require('request-ip').mw())
 
 const maxAgePeriod = 1000 * 60 * 60 * 24 * 7
 app.use(express.static('public', {
   maxAge: maxAgePeriod
 }))
+app.use('/', serveIndex('public'));
 
 module.exports = app;
