c3ec42b8d54ae4e040790358ef692f104e09532d
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2024-02-17 16:35:25 -0700

    Reduced client-side cache to 1 day.

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const serve_index_1 = __importDefault(require("serve-index"));
 const app = (0, express_1.default)();
-const maxAgePeriod = 1000 * 60 * 60 * 24 * 7;
+const maxAgePeriod = 1000 * 60 * 60 * 24 * 1;
 app.use(express_1.default.static('public', {
     maxAge: maxAgePeriod
 }));
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import serveIndex from 'serve-index'
 const app = express()
 
-const maxAgePeriod = 1000 * 60 * 60 * 24 * 7
+const maxAgePeriod = 1000 * 60 * 60 * 24 * 1
 app.use(express.static('public', {
   maxAge: maxAgePeriod
 }))
