c8a65471d5f51d3fc593ea59f3f980ba4511d47c
artem-v-shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2024-07-23 18:56:53 -0600

    Added missing video - original cut no longer available online.

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mime_types_1 = __importDefault(require("mime-types"));
 const serve_index_1 = __importDefault(require("serve-index"));
 const app = (0, express_1.default)();
 const maxAgePeriod = 1000 * 60 * 60 * 24 * 1;
 app.use(express_1.default.static('public', {
-    maxAge: maxAgePeriod
+    maxAge: maxAgePeriod,
+    setHeaders: (res, path) => {
+        if (mime_types_1.default.lookup(path) === 'video/mp4') {
+            res.setHeader('Cache-Control', 'public, max-age=31536000');
+        }
+    }
 }));
 app.use('/', (0, serve_index_1.default)('public'));
 module.exports = app;
